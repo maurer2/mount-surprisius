505b69c969f6677f79688aa983c41df40e2440b8
Martin <mr.mmaurer@gmail.com>
2026-06-06 00:47:02 +0200

    Added contrived type change example

---
+++ src/features/home/components/TypeChange/index.tsx
@@ -0,0 +1,56 @@
+import { useEffect, useReducer } from 'react';
+
+import './styles.css';
+
+type ToggleButtonProps = {
+  isActive: boolean;
+  onClick: () => void;
+};
+
+function ToggleButton({ isActive, onClick }: ToggleButtonProps) {
+  return (
+    <button type="button" aria-pressed={isActive} onClick={onClick} className="toggle-button">
+      List type
+    </button>
+  );
+}
+
+type ListContentProps = {
+  items: string[];
+};
+
+function ListContent({ items }: ListContentProps) {
+  useEffect(() => {
+    console.log('list mounted');
+  }, []);
+
+  return (
+    <>
+      {items.map((item) => (
+        <li key={item}>{item}</li>
+      ))}
+    </>
+  );
+}
+
+export function TypeChange() {
+  const [isUnordered, toggleIsUnordered] = useReducer((isUnordered) => !isUnordered, true);
+
+  return (
+    <article className="type-change">
+      <ToggleButton isActive={isUnordered} onClick={toggleIsUnordered} />
+
+      <h3>Remounts on type change of wrapping element</h3>
+      <p>Text</p>
+      {isUnordered ? (
+        <ul className="list">
+          <ListContent items={['Item 1', 'Item 2', 'Item 3']} />
+        </ul>
+      ) : (
+        <ol className="list">
+          <ListContent items={['Item 1', 'Item 2', 'Item 3']} />
+        </ol>
+      )}
+    </article>
+  );
+}
--- src/features/home/index.tsx
+++ src/features/home/index.tsx
@@ -1,3 +1,4 @@
+import { TypeChange } from './components/TypeChange';
 import { PositionalChange } from './components/PositionalChange';
 
 export function HomePage() {
@@ -10,6 +11,10 @@ export function HomePage() {
       <section aria-labelledby="positional-change-headline">
         <h2 id="positional-change-headline">Change of order of components</h2>
         <PositionalChange />
+      </section>
+      <section aria-labelledby="change-of-component-type-headline">
+        <h2 id="change-of-component-type-headline">Change of component type</h2>
+        <TypeChange />
       </section>
     </main>
   );
